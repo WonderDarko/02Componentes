aa3f75d8a4ae434465dc0c73445b47e9b99bd820
WonderDarko <wonder.montiel@gmail.com>
2022-09-19 15:40:14 -0500

    primer push

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Componentes en Vue</h1>
+  <hr/>
+  <card url="https://cdn.svgporn.com/logos/vue.svg" nombre="Vue.js"/>
+  <card url="https://cdn.svgporn.com/logos/angular-icon.svg" nombre="Angular"/>
+  <card url="https://cdn.svgporn.com/logos/vuetifyjs.svg" nombre="Vuetify"/>
+  <card url="https://cdn.svgporn.com/logos/react.svg" nombre="React"/>
+</template>
+
+<script>
+  import Card from "./components/Card.vue";
+  export default{
+    components: { Card },
+  };
+</script>
