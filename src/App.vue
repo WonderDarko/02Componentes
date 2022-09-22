b9d076906e9f254706e7368d74545678879656dd
WonderDarko <wonder.montiel@gmail.com>
2022-09-21 22:48:03 -0500

    practicas

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,32 @@
   <card url="https://cdn.svgporn.com/logos/angular-icon.svg" nombre="Angular"/>
   <card url="https://cdn.svgporn.com/logos/vuetifyjs.svg" nombre="Vuetify"/>
   <card url="https://cdn.svgporn.com/logos/react.svg" nombre="React"/>
+  <br>
+  <contador nombre-clase="btn btn-info"></contador>
+  <contador nombre-clase="btn btn-danger"></contador>
+  <contador nombre-clase="btn btn-success"></contador>
+  <br>
+  <modal>
+    <template v-slot:header>
+      <h5 class="modal-title" id="exampleModalLabel">Hola Mundo</h5>
+    </template>
+    <p>Esto es un contenido para nuestro modal</p>
+    <template v-slot:footer>
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      <button type="button" class="btn btn-primary">Enviar</button>
+    </template>
+  </modal>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+  </button>
 </template>
 
 <script>
   import Card from "./components/Card.vue";
+  import Contador from "./components/Contador.vue";
+  import Modal from "./components/Modal.vue";
   export default{
-    components: { Card },
+    components: { Card, Contador, Modal },
+    
   };
 </script>
---
+++ src/components/Contador.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        data: () => ({
+            count : 0,
+        }),
+        props:{
+            nombreClase:String
+        }
+    };
+</script>
+<template>
+    <button :class="nombreClase" v-on:click="count++"> Click {{count}} veces</button>
+</template>
